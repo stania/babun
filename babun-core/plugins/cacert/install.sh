cac2b3877c3bbe227ba43c5f6901b80984f913f5
stania (Won-gune Hwang) <stania.pe.kr@gmail.com>
2014-06-22 02:15:19 +0900

    fixing cacert installation problem

--- babun-core/plugins/cacert/install.sh
+++ babun-core/plugins/cacert/install.sh
@@ -4,5 +4,5 @@ source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
 cd /usr/ssl/certs
-curl -4 http://curl.haxx.se/ca/cacert.pem | /bin/awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
+curl -4 http://curl.haxx.se/ca/cacert.pem | /bin/gawk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
 c_rehash
